0287da9393cb7bcbfb6698e3ba889142e3160eb0
Jacob <akintolajacob75@gmail.com>
2026-09-14 16:42:49 +0100

    feat: register AddCityScreen in the AppNavigator

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SavedCitiesScreen from '../screens/SavedCitiesScreen';
+import AddCityScreen from '../screens/AddCityScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -7,6 +8,7 @@ function AppNavigator() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="SavedCities" component={SavedCitiesScreen} />
+      <Stack.Screen name="AddCity" component={AddCityScreen} />
     </Stack.Navigator>
   );
 }
