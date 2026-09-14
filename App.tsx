17ba03684d31114511fe816c7f750cd1dca12e4d
Jacob <akintolajacob75@gmail.com>
2026-09-14 16:35:26 +0100

    feat: add AddCityScreen component

--- App.tsx
+++ App.tsx
@@ -37,6 +37,7 @@
 // });
 
 // export default App;
+
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
---
+++ src/screens/AddCityScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+function AddCityScreen() {
+    return (
+        <View>
+            <Text>Add City</Text>
+        </View>
+    );
+}
+
+export default AddCityScreen;
