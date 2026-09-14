ebb216915e9ffbddf8b49ba3d224579722514712
Jacob <akintolajacob75@gmail.com>
2026-09-14 17:19:58 +0100

    feat: create the Weather Detail screen

---
+++ src/screens/WeatherDetailScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+function WeatherDetailScreen() {
+    return (
+        <View>
+            <Text>Weather Detail</Text>
+        </View>
+    );
+}
+
+export default WeatherDetailScreen;
