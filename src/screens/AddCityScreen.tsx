615fdc68724dad664923b23fda5cb4a7a2cfc758
Jacob <akintolajacob75@gmail.com>
2026-09-15 15:37:56 +0100

    feat: add functional back navigation to AddCityScreen

--- src/screens/AddCityScreen.tsx
+++ src/screens/AddCityScreen.tsx
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 function AddCityScreen() {
+    const navigation = useNavigation();
+
     return (
-        <View>
-            <Text>Add City</Text>
-        </View>
+      <View>
+        <Text>Add City</Text>
+        <Pressable
+          onPress={() => {
+            navigation.goBack();
+          }}
+        >
+          <Text>Go back</Text>
+        </Pressable>
+      </View>
     );
 }
 
